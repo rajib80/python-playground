2d75cb38ad1c3277d22b6a917fd5770ff9ba9847
rajib <rajib.shafiuzzaman@gmail.com>
2019-08-16 18:59:24 +0600

    find non-duplicate number in array

---
+++ find-the-non-duplicate-number/find-the-non-duplicate-number.py
@@ -0,0 +1,20 @@
+def find_single_number(numbers):
+    result = numbers[0]
+    print(result)
+    for i in range(1, len(numbers)):
+        result = result ^ numbers[i]
+        print(result)
+    
+    return result
+
+
+input_numbers = [4, 3, 2, 4, 1, 3, 2]
+print("Element occuring once is", find_single_number(input_numbers))
+
+input_numbers = [2, 3, 5, 4, 5, 3, 4] 
+print("Element occuring once is", find_single_number(input_numbers))
+
+
+
+
+
